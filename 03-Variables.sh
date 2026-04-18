518ea4873a130e58e520dea771c42cc3e753ca9e
Venkat-Tholeti <naidutholeti12@gmail.com>
2026-04-18 16:55:53 +0530

    Variables

--- 03-Variables.sh
+++ 03-Variables.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-Person1 = Modi
-Person2 = Trump
-Person3 = Kim Mama
-echo "$Person1: Hi $Person2, How are You ?"
-echo "$Person2: Hi $Person1, I am fine !"
-echo "$Person3: Hi $Person1 & $Person2, Should i bomb the world ?"
-echo "$Person2: Lets start war !"
-echo "$Person1: No !"
+PERSON1 = Modi
+PERSON2 = Trump
+PERSON3 = Kim Mama
+
+echo "$PERSON1: Hi $PERSON2, How are You ?"
+echo "$PERSON2: Hi $PERSON1, I am fine !"
+echo "$PERSON3: Hi $PERSON1 & $PERSON2, Should i bomb the world ?"
+echo "$PERSON2: Lets start war !"
+echo "$PERSON1: No !"
