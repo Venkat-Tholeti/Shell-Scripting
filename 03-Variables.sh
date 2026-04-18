51b24118f65a78b19f7f1965b04180ba2c76fd7a
Venkat-Tholeti <naidutholeti12@gmail.com>
2026-04-18 16:50:44 +0530

    Converstaions

---
+++ 03-Variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1 = Modi
+Person2 = Trump
+Person3 = Kim Mama
+echo "$Person1: Hi $Person2, How are You ?"
+echo "$Person2: Hi $Person1, I am fine !"
+echo "$Person3: Hi $Person1 & $Person2, Should i bomb the world ?"
+echo "$Person2: Lets start war !"
