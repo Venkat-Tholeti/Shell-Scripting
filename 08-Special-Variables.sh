561f390c601254975a72207994b4b33c16b3d387
Venkat-Tholeti <naidutholeti12@gmail.com>
2026-04-18 18:54:48 +0530

    Special Variables

---
+++ 08-Special-Variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Script name is $0"
+echo "Number of arguments passed $#"
+echo "PID of the script is $$"
+echo "Arguments passed are $@"
+echo "Status of last command is $?"
+echo "PID of the last command running in background is $!"
