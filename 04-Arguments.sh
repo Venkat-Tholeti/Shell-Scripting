ae36bc96ff895fd03aa5ca3a5e12590620ae64fb
Venkat-Tholeti <naidutholeti12@gmail.com>
2026-04-18 17:33:29 +0530

    Arguments

--- 04-Arguments.sh
+++ 04-Arguments.sh
@@ -6,3 +6,16 @@ echo "$2: Hi $1, I am fine !"
 echo "$3: Hi $1 & $2, Should i bomb the world ?"
 echo "$2: Lets start war!"
 echo "$1: No!"
+
+
+# OR
+
+# PERSON1=$1 #Here there should be no space before and after =
+# PERSON2=$2
+# PERSON3=$3
+
+# echo "$PERSON1: Hi $PERSON2, How are You ?"
+# echo "$PERSON2: Hi $PERSON1, I am fine !"
+# echo "$PERSON3: Hi $PERSON1 & $PERSON2, Should i bomb the world ?"
+# echo "$PERSON2: Lets start war!"
+# echo "$PERSON1: No!"
