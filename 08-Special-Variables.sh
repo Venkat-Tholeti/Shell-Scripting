615254deb8688c91695093756a01079c8dc509c1
Venkat-Tholeti <naidutholeti12@gmail.com>
2026-04-18 19:00:46 +0530

    Special Variables

--- 08-Special-Variables.sh
+++ 08-Special-Variables.sh
@@ -7,3 +7,7 @@ echo "Arguments passed are $@"
 echo "Status of last command is $?"
 sleep 10 &
 echo "PID of the last command running in background is $!"
+echo "Present Directory $PWD"
+echo "Home Directory $HOME"
+echo "User Running the Script is $USER"
+echo "Script Executed at $(date)"
