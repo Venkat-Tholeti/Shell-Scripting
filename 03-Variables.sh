0f68c59297987eb6c45e562c504bbf59b6ad42ea
Venkat-Tholeti <naidutholeti12@gmail.com>
2026-04-18 16:56:50 +0530

    Variables

--- 03-Variables.sh
+++ 03-Variables.sh
@@ -4,8 +4,8 @@ PERSON1 = Modi
 PERSON2 = Trump
 PERSON3 = Kim Mama
 
-echo "$PERSON1: Hi $PERSON2, How are You ?"
-echo "$PERSON2: Hi $PERSON1, I am fine !"
-echo "$PERSON3: Hi $PERSON1 & $PERSON2, Should i bomb the world ?"
-echo "$PERSON2: Lets start war !"
-echo "$PERSON1: No !"
+echo "$PERSON1:: Hi $PERSON2, How are You ?"
+echo "$PERSON2:: Hi $PERSON1, I am fine !"
+echo "$PERSON3:: Hi $PERSON1 & $PERSON2, Should i bomb the world ?"
+echo "$PERSON2:: Lets start war !"
+echo "$PERSON1:: No !"
