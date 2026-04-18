8385600d92e3e60eb600813a8dc0fe6de72dfa96
Venkat-Tholeti <naidutholeti12@gmail.com>
2026-04-18 20:10:59 +0530

    Install Command

--- 10-Install-Command.sh
+++ 10-Install-Command.sh
@@ -5,6 +5,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then  
   echo "ERROR: Please run this script with root access"
+  exit 1 # we can give any number upto 127 other than 0 
 else
   echo "You are Running with root access"
 fi
