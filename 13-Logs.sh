99edbbbbffa0911949605880d28bd4ed232054a3
Venkat-Tholeti <naidutholeti12@gmail.com>
2026-04-20 22:41:56 +0530

    logs

--- 13-Logs.sh
+++ 13-Logs.sh
@@ -15,23 +15,23 @@ LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
 
-echo "$G Script Started Executing At $(date) $N"
+echo -e "$G Script Started Executing At $(date) $N" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]
 then  
-  echo -e  "$R ERROR: Please run this script with root access $N" &>>$LOG_FILE
+  echo -e  "$R ERROR: Please run this script with root access $N" | tee -a $LOG_FILE
   exit 1 # we can give any number upto 127 other than 0 
 else
-  echo -e  "$G You are Running with root access $N" &>>$LOG_FILE
+  echo -e  "$G You are Running with root access $N" | tee -a $LOG_FILE
 fi
 
 #FUNCTION FOR REPEATED CODE
 VALIDATE(){
     if [ $1 -eq 0 ] # We can pass arguments to function also , Here $1 is 1st argument ($?) of VALIDATE in Installed space below
   then
-    echo -e   "$G Installation of $2 is success $N" &>>$LOG_FILE # $2 is Software mentioned as below in VALIDATE function in INSTALLED SPACE BELOW
+    echo -e   "$G Installation of $2 is success $N" | tee -a $LOG_FILE # $2 is Software mentioned as below in VALIDATE function in INSTALLED SPACE BELOW
   else
-    echo -e  "$R Installation of $2 is failure $N" &>>$LOG_FILE
+    echo -e  "$R Installation of $2 is failure $N" | tee -a $LOG_FILE
     exit 1
   fi
 
@@ -42,26 +42,26 @@ dnf list installed mysql
 #This will check if already installed or not, if not it will install
 if [ $? -ne 0 ]
 then
-  echo -e "$Y Mysql is not installed, Installation will Proceed $N" &>>$LOG_FILE
+  echo -e "$Y Mysql is not installed, Installation will Proceed $N" | tee -a $LOG_FILE
   dnf install mysql -y
   #This is install status after it is being installed
   VALIDATE $? "MYSQL" #here $1 = $? (1st argument) $2 = MYSQL (2nd Argument) 
 else
-   echo -e "$N MYSQL is already installed , Nothing to do! $N" &>>$LOG_FILE
+   echo -e "$N MYSQL is already installed , Nothing to do! $N" | tee -a $LOG_FILE
 fi
 
 dnf list installed python3
 #This will check if already installed or not, if not it will install
 if [ $? -ne 0 ]
 then
-  echo -e "$Y python3 is not installed, Installation will Proceed $N" &>>$LOG_FILE
+  echo -e "$Y python3 is not installed, Installation will Proceed $N" | tee -a $LOG_FILE
   #If  not installed proceed with installation
   dnf install python3 -y
   #This is install status after it is being installed
   VALIDATE $? "Python3" #here $1 = $? (1st argument) $2 = Python3 (2nd Argument)
  
 else
-   echo -e "$N Python3 is already installed , Nothing to do! $N" &>>$LOG_FILE
+   echo -e "$N Python3 is already installed , Nothing to do! $N" | tee -a $LOG_FILE
 fi
 
 
@@ -69,12 +69,12 @@ dnf list installed nginx
 #This will check if already installed or not, if not it will install
 if [ $? -ne 0 ]
 then
-  echo -e "$Y Nginx is not installed, Installation will Proceed $N" &>>$LOG_FILE
+  echo -e "$Y Nginx is not installed, Installation will Proceed $N" | tee -a $LOG_FILE
   #If  not installed proceed with installation
   dnf install nginx -y
    #This is install status after it is being installed
   VALIDATE $? "Nginx" #here $1 = $? (1st argument) $2 = Nginx (2nd Argument)
 
 else
-   echo -e "$N Nginx is already installed , Nothing to do! $N" &>>$LOG_FILE
+   echo -e "$N Nginx is already installed , Nothing to do! $N" | tee -a $LOG_FILE
 fi
