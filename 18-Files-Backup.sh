3ed9dc568b8b125fd6eff2b4e4414db53fab9d73
Venkat-Tholeti <naidutholeti12@gmail.com>
2026-05-01 21:08:47 +0530

    Shell_scripting

--- 18-Files-Backup.sh
+++ 18-Files-Backup.sh
@@ -2,7 +2,7 @@
 
 SOURCE_DIRECTORY="/home/ec2-user/Shell-Scripting/apps/logs/"
 DESTINATION_DIRECTORY="/home/ec2-user/Shell-Scripting/backup/logs"
-DAYS=${3:+14} #if days are provided argument is considered, or else by default 14 days
+DAYS=${3:-14} #if days are provided argument is considered, or else by default 14 days
 
 mkdir -p $DESTINATION_DIRECTORY  #IF NOT PRESENT, IT WILL CREATE, IF PRESENT IT WILL IGNORE
 
