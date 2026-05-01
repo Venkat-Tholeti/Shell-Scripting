9227fbe434cb48965cf8f80124e76ae103fcf942
Venkat-Tholeti <naidutholeti12@gmail.com>
2026-05-01 21:18:19 +0530

    Shell_scripting

--- 18-Files-Backup.sh
+++ 18-Files-Backup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# SOURCE_DIRECTORY="/home/ec2-user/Shell-Scripting/apps/logs/"
-# DESTINATION_DIRECTORY="/home/ec2-user/Shell-Scripting/backup/logs"
-# #DAYS=${3:-14} #if days are provided argument is considered, or else by default 14 days
+SOURCE_DIRECTORY="/home/ec2-user/Shell-Scripting/apps/logs/"
+DESTINATION_DIRECTORY="/home/ec2-user/Shell-Scripting/backup/logs"
+#DAYS=${3:-14} #if days are provided argument is considered, or else by default 14 days
 
-# mkdir -p $DESTINATION_DIRECTORY  #IF NOT PRESENT, IT WILL CREATE, IF PRESENT IT WILL IGNORE
+mkdir -p $DESTINATION_DIRECTORY  #IF NOT PRESENT, IT WILL CREATE, IF PRESENT IT WILL IGNORE
 
-# find $SOURCE_DIRECTORY -type f -mtime +14 -exec cp {} $DESTINATION_DIRECTORY \; #{} this means current file
+find $SOURCE_DIRECTORY -type f -mtime +14 -exec cp {} $DESTINATION_DIRECTORY \; #{} this means current file
 
-# echo "Backup Completed Successfully"
+echo "Backup Completed Successfully"
 
 # IF WE NEED TO TAR THE FILES THEN 
 
@@ -20,6 +20,9 @@ mkdir -p $DESTINATION_DIRECTORY  #IF NOT PRESENT, IT WILL CREATE, IF PRESENT IT 
 
 find $SOURCE_DIRECTORY -type f -mtime +14 | tar -czf $DESTINATION_DIRECTORY/backup_$DATE.tar.gz -T -
 
-echo "Backup Completed Successfully"
+echo "Zipping  Completed Successfully"
 
 #HERE -T - means it will take input from find
+
+# to check files are there in tar.gz
+#tar -tzf backup_2026-05-01.tar.gz
