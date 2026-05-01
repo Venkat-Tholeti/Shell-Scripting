ff5eab68aee3d30470551b4b4288f13738d61ea6
Venkat-Tholeti <naidutholeti12@gmail.com>
2026-05-01 20:27:35 +0530

    Shell_scripting

---
+++ 17-Delete-OldLogs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SOURCE_DIRECTORY=/home/ec2-user/Shell-Scripting/apps/logs
+Files_TO_DELETE=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
+
+while read line
+do
+    rm -rf $line
+done <<< $Files_TO_DELETE  
+
+#FOR INPUT IS FROM FILE OUTSIDE WE WILL GIVE < , BUT INPUT FROM WITHIN SCRIPT <<<
+
+
