21cdf1d980c005843646b544b117d1db52ea10e9
Venkat-Tholeti <naidutholeti12@gmail.com>
2026-05-01 20:31:16 +0530

    Shell_scripting

--- 17-Delete-OldLogs.sh
+++ 17-Delete-OldLogs.sh
@@ -6,7 +6,7 @@ Files_TO_DELETE=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
 while read line
 do
     rm -rf $line
-    echo "Files Deleted"
+    echo "$line Files Deleted "
 done <<< $Files_TO_DELETE  
 
 #FOR INPUT IS FROM FILE OUTSIDE WE WILL GIVE < , BUT INPUT FROM WITHIN SCRIPT <<<
