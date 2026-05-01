a77dcc77ae540ea4ac7c7af80e6fc48c64939053
Venkat-Tholeti <naidutholeti12@gmail.com>
2026-05-01 21:09:48 +0530

    Shell_scripting

--- 18-Files-Backup.sh
+++ 18-Files-Backup.sh
@@ -2,10 +2,10 @@
 
 SOURCE_DIRECTORY="/home/ec2-user/Shell-Scripting/apps/logs/"
 DESTINATION_DIRECTORY="/home/ec2-user/Shell-Scripting/backup/logs"
-DAYS=${3:-14} #if days are provided argument is considered, or else by default 14 days
+#DAYS=${3:-14} #if days are provided argument is considered, or else by default 14 days
 
 mkdir -p $DESTINATION_DIRECTORY  #IF NOT PRESENT, IT WILL CREATE, IF PRESENT IT WILL IGNORE
 
-find $SOURCE_DIRECTORY -type f -mtime $DAYS -exec cp {} $DESTINATION_DIRECTORY \; #{} this means current file
+find $SOURCE_DIRECTORY -type f -mtime +14 -exec cp {} $DESTINATION_DIRECTORY \; #{} this means current file
 
 echo "Backup Completed Successfully"
